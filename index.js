7b387db8089bbc4a61b6a0a5f4ac777d61aa9bba
Firass Ziedan <firass.ziedan@gmail.com>
2020-03-27 15:48:19 +0300

    Add Drupal stack packs file

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const stackPacks = [
   require('./packs/react.js'),
   require('./packs/angular.js'),
   require('./packs/amp.js'),
-  require('./packs/magento.js')
+  require('./packs/magento.js'),
+  require('./packs/drupal.js')
 ];
 
 module.exports = stackPacks;
